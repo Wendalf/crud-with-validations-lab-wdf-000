f99ce9a5a118f68a99b07694a98bf2d4e48f18e8
Wendy Xu <guangw.xu@gmail.com>
2016-09-22 13:24:08 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,5 @@
 class Song < ActiveRecord::Base
-  validates :title, presence: true, uniqueness: {scope: [:artist_name, :release_year]}
+  validates :title, presence: true, uniqueness: {scope: [:artist_name, :release_year], message: "cannot be repeated by the same artist in the same year"}
   validates :released, inclusion: {in: [true, false]}
   validates :artist_name, presence: true
   validate :release_year_custom
@@ -11,5 +11,18 @@ class Song < ActiveRecord::Base
       errors.add(:release_year, "can not be in the future")
     end
   end
-  
+
+  ###solution for release_year_custom
+
+  # with_options if: :released? do |song|
+  #   song.validates :release_year, presence: true
+  #   song.validates :release_year, numericality: {
+  #     less_than_or_equal_to: Date.today.year
+  #   }
+  # end
+
+  # def released?
+  #   released
+  # end
+
 end
